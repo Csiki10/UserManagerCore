77f533704a01deefa48fb2fbf49f5842613b9036
benedek.csikos <csikosbenedek@gmail.com>
2024-08-29 14:55:22 +0200

    display attributes added

--- ViewModels/EditUserViewModel.cs
+++ ViewModels/EditUserViewModel.cs
@@ -7,22 +7,31 @@ namespace UserManagerCore.ViewModels
         public int ID { get; set; }
 
         [Required]
+        [Display(Name = "Username")]
         public string Username { get; set; }
 
         [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
         public DateTime DateOfBirth { get; set; }
 
         [Required]
+        [Display(Name = "Place of Birth")]
         public string PlaceOfBirth { get; set; }
 
         [Required]
+        [Display(Name = "Place of Residence")]
         public string PlaceOfResidence { get; set; }
+
+        public IEnumerable<string> PlacesOfBirth { get; set; }
+        public IEnumerable<string> PlacesOfResidence { get; set; }
     }
 }
--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -4,9 +4,11 @@ namespace UserManagerCore.ViewModels
 {
     public class LoginViewModel
     {
+        [Display(Name = "User Name")]
         [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
 
+        [Display(Name = "Password")]
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
     }
